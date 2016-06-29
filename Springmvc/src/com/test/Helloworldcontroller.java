5e8053d7e29ebe9126df9cdaa68397a7cb79161a
AkshayJain52 <akshayjain308@gmail.com>
2016-06-29 17:58:30 +0530

    ht

---
+++ Springmvc/src/com/test/Helloworldcontroller.java
@@ -0,0 +1,13 @@
+package com.test;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.portlet.ModelAndView;
+@Controller
+public class Helloworldcontroller {
+	@RequestMapping("/hello")
+	public ModelAndView helloWorld() {  
+        String message = "HELLO SPRING MVC HOW R U";  
+        return new ModelAndView("hellopage", "message", message); 
+}
+}
