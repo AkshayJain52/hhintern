3ca5d5bc45f5cb41ed7bc2782ad1975c79dfb780
AkshayJain52 <akshayjain308@gmail.com>
2016-08-01 13:56:56 +0600

    ok

--- CounterWebApp/src/main/java/com/mkyong/controller/mastercontroller.java
+++ CounterWebApp/src/main/java/com/mkyong/controller/mastercontroller.java
@@ -1,21 +1,27 @@
 package com.mkyong.controller;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
-//import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import javax.validation.Valid;
 
+import org.jdom2.JDOMException;
+import org.jdom2.input.SAXBuilder;
+import org.jdom2.output.XMLOutputter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -25,6 +31,11 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.pdf.PdfWriter;
+import com.itextpdf.tool.xml.XMLWorkerHelper;
 
 @Controller
 public class mastercontroller{
@@ -79,6 +90,11 @@ public class mastercontroller{
 		if(error){
 			return "index";
 		}
+		
+		
+		
+		
+		
 		//email_id = request.getParameter("email_id");
 		//indexvalidation iv = new indexvalidation();
 		//String error = iv.validate();
@@ -99,55 +115,113 @@ public class mastercontroller{
 	}
 	//response body for handling the download of pdf
 	@RequestMapping(value= "/download_pdf",method = RequestMethod.GET)
-	public String downloadpdf(HttpServletRequest request,HttpServletResponse response) throws IOException  {
+	public String downloadpdf(HttpServletRequest request,HttpServletResponse response) throws IOException, JDOMException, DocumentException  {
 
 		filename="Resume_"+First_name.toUpperCase()+".pdf";
-		ClassLoader classLoader = getClass().getClassLoader();
-		File file = new File(classLoader.getResource("pdf.exe").getFile());
-		String absolutePath = file.getAbsolutePath();
-		
-	ProcessBuilder pb = new ProcessBuilder( absolutePath,url,filename);
-    pb.redirectErrorStream(true);
-    
-    BufferedReader inStreamReader = new BufferedReader(new InputStreamReader(pb.start().getInputStream()));
-   
-    while(inStreamReader.readLine() != null)
-            {            
-               inStreamReader.readLine();
-            }
     		
     		//give the user downloaded file
           
-           response.setContentType("pdf/docx");
-           PrintWriter out = response.getWriter();     
-           response.setContentType("APPLICATION/OCTET-STREAM");   
-           response.setHeader("Content-Disposition","attachment; filename=\"" + filename + "\"");  
-           System.out.println(filename + " is created!");
-           FileInputStream fileInputStream = new FileInputStream(filename);
-           int i1;
-           while ((i1=fileInputStream.read()) != -1) {  
-           out.write(i1);
-           }
-           fileInputStream.close();
-           out.close();
-                  
-           // delete the file by giving the file to user
-           try{
-       		
-       		File file1 = new File(filename);
-       		if(file1.delete()){
-       			System.out.println(file1.getName() + " is deleted!");
-       		}else{
-       			System.out.println("Delete operation is failed.");
-       		}
-       	   
-       	}catch(Exception e){
-       		
-       		e.printStackTrace();
-       		}
-		return "index";
+    try {
+		   //----------------------- HTML CREATTION ------------------------
+		   // get URL content
+    		URL yahoo = new URL(url);
+		   URLConnection conn = yahoo.openConnection();
+		   
+		   // open the stream and put it into BufferedReader
+		   BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+		   String inputLine;
+		 
+		   //save to this filename
+		   String fileName = "temp.html";
+		   File file1 = new File(fileName);
+		   if (!file1.exists()) {
+		    file1.createNewFile();
+		   }
+		   //use FileWriter to write file
+		   FileWriter fw = new FileWriter(file1.getAbsoluteFile());
+		   BufferedWriter bw = new BufferedWriter(fw);
+		   while ((inputLine = br.readLine()) != null) {
+		    bw.write(inputLine);
+		   }
+		   bw.close();
+		   br.close();
+		   //----------------------- HTML CREATTION ------------------------
+		   
+		   //----------------------- HTML TO XML CREATTION ------------------------   
+		   FileWriter fwOutXml =null;
+		   FileReader frInHtml=null;
+		   BufferedWriter bwOutXml =null;
+		   BufferedReader brInHtml=null;
+		   
+		   frInHtml = new FileReader("temp.html");
+		      brInHtml = new BufferedReader(frInHtml);
+		      @SuppressWarnings("deprecation")
+			SAXBuilder saxBuilder = new SAXBuilder("org.ccil.cowan.tagsoup.Parser", false);
+		      org.jdom2.Document jdomDocument = saxBuilder.build(brInHtml);
+		      XMLOutputter outputter = new XMLOutputter();
+		   
+		      outputter.output(jdomDocument, System.out);
+		         fwOutXml = new FileWriter("xml.xml");
+		         bwOutXml = new BufferedWriter(fwOutXml);
+		         outputter.output(jdomDocument, bwOutXml);
+		         System.out.flush();
+		          
 
-	}
+		         fwOutXml.flush();
+		         fwOutXml.close();
+		         bwOutXml.close();
+		           //----------------------- HTML TO XML CREATTION ------------------------   
+		       
+		         //----------------------- XML TO PDF CREATTION ------------------------   
+		         Document document = new Document();
+		         PdfWriter writer = PdfWriter.getInstance(document, new FileOutputStream(filename));
+		         document.open();
+		         XMLWorkerHelper.getInstance().parseXHtml(writer, document,new FileInputStream("xml.xml"));        
+		         document.close();
+		        
+		         //----------------------- XML TO PDF CREATTION ------------------------ 
+		         
+		         File html_temp_file = new File("temp.html");
+		         File xml_temp_file = new File("xml.xml");
+		         xml_temp_file.delete();
+		         html_temp_file.delete();
+		          
+		 
+		  } catch (MalformedURLException e1) {
+		   e1.printStackTrace();
+		  } catch (IOException e) {
+		   e.printStackTrace();
+		  }
+		 
+    response.setContentType("pdf/docx");
+    PrintWriter out1 = response.getWriter();
+    response.setContentType("APPLICATION/OCTET-STREAM");
+    response.setHeader("Content-Disposition","attachment; filename=\"" +filename+ "\"");   
+
+    FileInputStream fileInputStream1 = new FileInputStream(filename);
+
+    int i1;
+    while ((i1=fileInputStream1.read()) != -1) {
+    out1.write(i1);
+    }
+    fileInputStream1.close();
+    out1.close();
+    
+    
+ // delete the file by giving the file to user
+    try{
+   		
+   		File file1 = new File(filename);
+   		if(file1.delete());
+   	  }catch(Exception e)
+    {
+   		  e.printStackTrace();
+    }
+	return "index";
+}
+	
+	
+	
 
 		// response body for handling the download of docx
 		@RequestMapping(value= "/download_docx",method = RequestMethod.GET)
@@ -164,7 +238,7 @@ public class mastercontroller{
 			  StringBuilder a = new StringBuilder();
 			  while ((inputLine = in1.readLine()) != null)
 			     a.append(inputLine);
-			  System.out.println(filename1 + " is created!");
+			 
 			  FileOutputStream fout=new FileOutputStream(filename1);
 			  byte b[]=a.toString().getBytes();//converting string into byte array
 			  fout.write(b);
@@ -191,11 +265,7 @@ public class mastercontroller{
            try{
           		
           		File file = new File(filename1);
-          		if(file.delete()){
-          			System.out.println(file.getName() + " is deleted!");
-          		}else{
-          			System.out.println("Delete operation is failed.");
-          		}
+          		if(file.delete());
           	  }catch(Exception e){
           		  						e.printStackTrace();
           						 }
